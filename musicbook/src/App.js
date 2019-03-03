203f30b10b7eea641ece5a7797fc2e546ef26f68
jj888 <jaconjcondes@gmail.com>
2019-03-02 23:27:57 -0800

    Working Fake Data Display

--- musicbook/src/App.js
+++ musicbook/src/App.js
@@ -2,7 +2,45 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+const data = [
+   {
+      song_name: "Sad Song 1",
+      artist: "Jake's Band",
+      video_url: "https://www.youtube.com/watch?v=66C4YIiwRbM",
+      contributor: "Chris C",
+      likes: 8,
+      dislikes: 2
+   },
+   {
+      song_name: "Sad Song 2",
+      artist: "Jake's Band",
+      video_url: "https://www.youtube.com/watch?v=66C4YIiwRbM",
+      contributor: "Chris C",
+      likes: 24,
+      dislikes: 3
+   },
+   {
+      song_name: "Sad Song 3",
+      artist: "Jake's Band",
+      video_url: "https://www.youtube.com/watch?v=66C4YIiwRbM",
+      contributor: "Chris C",
+      likes: 25,
+      dislikes: 50
+   }
+];
+
 class App extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         data: data
+      };
+   }
+
+   showData = () => {
+      console.log(this.state.data);
+   };
+
    render() {
       return (
          <div className="App">
@@ -19,6 +57,7 @@ class App extends Component {
                >
                   Learn React
                </a>
+               <button onClick={this.showData}>Click Me</button>
             </header>
          </div>
       );
