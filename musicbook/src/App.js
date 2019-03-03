5337cad5031dac094f7cf5b3444dc6a351bc7a72
jj888 <jaconjcondes@gmail.com>
2019-03-02 23:33:03 -0800

    Working Fake Data Display

--- musicbook/src/App.js
+++ musicbook/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 const data = [
@@ -44,21 +43,9 @@ class App extends Component {
    render() {
       return (
          <div className="App">
-            <header className="App-header">
-               <img src={logo} className="App-logo" alt="logo" />
-               <p>
-                  Edit <code>src/App.js</code> and save to reload.
-               </p>
-               <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-               >
-                  Learn React
-               </a>
-               <button onClick={this.showData}>Click Me</button>
-            </header>
+            <button onClick={this.showData} className="myButton">
+               Click Me
+            </button>
          </div>
       );
    }
