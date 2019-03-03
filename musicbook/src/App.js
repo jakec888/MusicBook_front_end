c09ace587bd0a3cb1718f471c91b361e7ff77fe7
chef <cgcook81@gmail.com>
2019-03-03 02:02:53 -0500

    test display

--- musicbook/src/App.js
+++ musicbook/src/App.js
@@ -4,10 +4,9 @@ class App extends Component {
    render() {
       return (
          <div>
-          <h1>MusicBook... Facebook's 2nd cousin</h1>
+            <h1>MusicBook... Facebook's 2nd cousin</h1>
          </div>
-      );
+      )
    }
 }
-
 export default App;
