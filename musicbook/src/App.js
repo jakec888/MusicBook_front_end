ce500d33a0683c48c29139b8f714aed774976889
chef <cgcook81@gmail.com>
2019-03-03 13:39:19 -0500

    test react -successful fetch

--- musicbook/src/App.js
+++ musicbook/src/App.js
@@ -1,10 +1,38 @@
 import React, { Component } from "react";
 
+class Header extends Component {
+  render() {
+    return (
+      <h1>MusicBook... Facebook's 2nd cousin!</h1>
+    )
+  }
+}
+// class Form extends Component {
+//   contructor(props) {
+//      super(props)
+//      this.state = {
+//
+//      }
+//   }
+// }
+
+
 class App extends Component {
+  fetchSongs = () => {
+    fetch('http://localhost:3000/songs')
+      .then(data => data.json())
+      .then(music => {
+        console.log(music)
+      })
+      .catch(err => console.log(err))
+  }
+
+
    render() {
+     this.fetchSongs()
       return (
          <div>
-            <h1>MusicBook... Facebook's 2nd cousin</h1>
+            <Header />
          </div>
       )
    }
